7814e531eab2c16a396ebc34386c37b7d22553e8
Mark Reed <markdreed512@gmail.com>
2020-06-08 16:14:47 -0500

    fetched lat and long

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [IssPosition, setIssPosition] = useState({lat: 0, long: 0})
+  const getIssPosition = () => {
+    fetch('http://api.open-notify.org/iss-now.json')
+      .then(response => response.json())
+      .then(data => {
+        let lat = data.iss_position.latitude
+        let long = data.iss_position.longitude
+        setIssPosition({lat, long})
+      });
+  }
+  useEffect(getIssPosition, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      lat: {IssPosition.lat}
+      long: {IssPosition.long}
     </div>
   );
 }
