15f285da04ae30056ea53b9584713917e5cdf033
Mark Reed <markdreed512@gmail.com>
2020-06-16 21:21:13 -0500

    marker and icon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Map, TileLayer } from 'react-leaflet'
+import  { Map, TileLayer, Marker } from 'react-leaflet'
+import L from 'leaflet'
 function App() {
   const [IssPosition, setIssPosition] = useState({ lat: 0, long: 0 })
+  const [ ZoomLevel, setZoomLevel] = useState(5)
   const getIssPosition = () => {
     fetch('http://api.open-notify.org/iss-now.json')
       .then(response => response.json())
@@ -12,16 +14,33 @@ function App() {
         setIssPosition({ lat, long })
       });
   }
-  useEffect(getIssPosition, [])
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getIssPosition()
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+
+  const issIcon = new L.Icon({
+      iconUrl: require('./images/iss.png'),
+      iconSize: new L.Point(30, 30),
+      iconAnchor: [20,20],
+      className: 'leaflet-div-icon'
+  });
 
   return (
     <div className="App">
-      <Map center={[IssPosition.lat, IssPosition.long]}  zoom={1}>
+      
+      <Map center={[IssPosition.lat, IssPosition.long]}  zoom={ZoomLevel}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
+        <Marker position={[IssPosition.lat, IssPosition.long]} icon={issIcon} />
       </Map>
+      
     </div>
   );
 }
