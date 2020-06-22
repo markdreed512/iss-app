f744caa66ea7c86813da1fb93c8a900857081f93
Mark Reed <markdreed512@gmail.com>
2020-06-22 10:44:25 -0500

    formatted days, time in table

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,23 +1,54 @@
 import React from 'react'
+import Moment from 'moment'
+import { Table, TableHead, TableBody, TableRow, TableCell, TableContainer } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+      maxWidth: "40%"
+    },
+  });
 function Display(props) {
-    if(props.dates){
+    const classes = useStyles()
+    if (props.dates) {
         return (
             <div>
-                <h1>{`${props.lat} ${props.long} ${props.city}`}</h1>
-                
-                {props.dates.map((date, i)=>{
-                    const d = new Date(date.risetime * 1000)
-                    console.log(d.toString())
-                    return <li key={i}>{d.toString()}</li>
-                })}
+                <h1>{`Passes for ${props.city}. Times in UTC`}</h1>
+                <TableContainer>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                                <TableCell>Day</TableCell>
+                                <TableCell>Date</TableCell>
+                                <TableCell>Time</TableCell>
+                                <TableCell>Duration</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {props.dates.map((date, i) => {
+                            const d = Moment.unix(date.risetime).utc()
+                            // .format('dddd, MMMM Do, YYYY h:mm:ss A')
+                            return (
+                                <TableRow key={i}>
+                                    <TableCell>{d.format('ddd')}</TableCell>
+                                    <TableCell>{d.format('LT')}</TableCell>
+                                    <TableCell>{d.format('ddd')}</TableCell>
+                                    <TableCell>{d.format('LT')}</TableCell>
+                                    
+                                </TableRow>
+                            )
+                        })}
+                    </TableBody>
+                </Table>
+                </TableContainer>
                 {/* <h1>{`Pass times: ${if(dates){dates[0].risetime}, ${dates[1].risetime}, ${dates[2].risetime}`}</h1> */}
             </div>
         )
     } else {
         return null
     }
-    
+
 }
 
 export default Display
--- src/components/Input.js
+++ src/components/Input.js
@@ -8,8 +8,9 @@ function Input() {
     const [dates, setDates] = useState([])
 
     useEffect(() => {
+        const apiKey = "4b2ccb0553c346ef9b0782e991f3eb1d"
         if (searchQuery) {
-            const url = `https://api.opencagedata.com/geocode/v1/json?q=${searchQuery}&key=4b2ccb0553c346ef9b0782e991f3eb1d`
+            const url = `https://api.opencagedata.com/geocode/v1/json?q=${searchQuery}&key=${apiKey}`
             fetch(url)
                 .then(response => response.json())
                 .then(data => {
@@ -17,21 +18,18 @@ function Input() {
                     setLong(data.results[0].geometry.lng)
                     setCity(data.results[0].components.city)
                 })
-        }
-
+            }
     }, [searchQuery])
     useEffect(() => {
         if (lat && long) {
             //get iss pass times when lat or long update
-            const url = `https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${long}&n=5`
+            const url = `https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=${lat}&lon=${long}&n=70`
             fetch(url)
                 .then((response) => response.json())
                 .then((data) => {
-                    console.log(data.response)
                     setDates(data.response)
                 })
         }
-
     }, [lat, long])
 
     const handleSubmit = (e) => {
